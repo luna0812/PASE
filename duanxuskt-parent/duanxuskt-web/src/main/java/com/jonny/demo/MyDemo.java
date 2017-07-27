92e17f826aa23a920fe953177c1898b71337c30b
duanxu <duanxuskt@live.com>
2017-07-27 17:27:38 +0800

    duaxnu first commit

---
+++ duanxuskt-parent/duanxuskt-web/src/main/java/com/jonny/demo/MyDemo.java
@@ -0,0 +1,10 @@
+package com.jonny.demo;
+
+/**
+ * Created by dunxuskt on 2017/7/27.
+ */
+public class MyDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello,IDEA!");
+    }
+}
---
+++ duanxuskt-parent/duanxuskt-web/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>A Maven Project!</h2>
+</body>
+</html>
